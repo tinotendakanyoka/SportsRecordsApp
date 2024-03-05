6ba7bfba9d092f2056a219c8a6cc96dee7fa0ae1
Tinotenda Kanyoka <purplelaneofficial@gmail.com>
2024-03-05 12:40:38 +0000

    Edit Template

--- SportRecord/core/admin.py
+++ SportRecord/core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Student, Event, EventParticipation
+from .models import Student, Event, EventParticipation, House
 
 
 admin.site.register(Student)
 admin.site.register(Event)
 admin.site.register(EventParticipation)
+admin.site.register(House)
 
