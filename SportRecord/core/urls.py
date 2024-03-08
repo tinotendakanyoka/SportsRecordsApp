a1bc1eae1e89078acd05371f4f7725c7062d7ffc
Tinotenda Kanyoka <purplelaneofficial@gmail.com>
2024-03-08 09:48:46 +0000

    table

--- SportRecord/core/urls.py
+++ SportRecord/core/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='index'),
-    path('events/<event_name>/<gender>/<age_group>', views.CreateMultipleParticipationsView, name='events'),
+    # path('events/<event_name>/<gender>/<age_group>', views.CreateMultipleParticipationsView, name='events'),
+    path('events/<event_name>/<gender>/<age_group>', views.register_participants, name='register'),
     path('students', views.UpdateStudentsView.as_view(), name='students_update'),
 ]
