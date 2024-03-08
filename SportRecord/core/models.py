5ca9a9e09eba1a3bc9510a2caaeb98c889ef4921
Tinotenda Kanyoka <purplelaneofficial@gmail.com>
2024-03-08 13:26:09 +0000

    s

--- SportRecord/core/models.py
+++ SportRecord/core/models.py
@@ -65,7 +65,7 @@ class Student(models.Model):
 class Record(models.Model):
     description = models.CharField(max_length=255)
     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='records')
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='record')
+    event = models.ManyToManyField(Event, related_name='record')
     time_or_distance = models.CharField(max_length=255)
     event_year = models.IntegerField(default=timezone.now().year)
 
