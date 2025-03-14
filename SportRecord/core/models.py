c584d5c50e7c88254f210385affa666676ce54ae
Tinotenda Kanyoka <tkanyoka01@gmail.com>
2025-03-14 19:41:41 +0200

    Logic

--- SportRecord/core/models.py
+++ SportRecord/core/models.py
@@ -67,6 +67,9 @@ class Record(models.Model):
         return f'{self.participant} - {self.event} - {self.record}'
     
 
+from django.db import models
+from django.utils import timezone
+
 class EventParticipation(models.Model):
     event = models.ForeignKey(AthleticEvent, on_delete=models.CASCADE)
     participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
@@ -75,72 +78,78 @@ class EventParticipation(models.Model):
     attempt3 = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
 
     best_attempt = models.FloatField(default=0.00)
-    athlete_position = models.IntegerField(default=6)
+    athlete_position = models.IntegerField(default=0)
 
     class Meta:
         unique_together = ['event', 'participant']
 
     def save(self, *args, **kwargs):
+        """ Update best attempt before saving """
         self.best_attempt = max(self.attempt1, self.attempt2, self.attempt3)
-        super().save(*args, **kwargs)  # Save the current instance first
+        super().save(*args, **kwargs)
 
-        # Update athlete positions and points
-        all_event_participants = EventParticipation.objects.filter(event=self.event).order_by('-best_attempt')
+        # **Update rankings efficiently**
+        all_event_participants = list(EventParticipation.objects.filter(event=self.event).order_by('-best_attempt'))
+        
+        # Assign rankings
         for index, event_participant in enumerate(all_event_participants):
             event_participant.athlete_position = index + 1
 
-            # Update points for house and individual
-            if event_participant.athlete_position == 1:
-                if event_participant.best_attempt > event_participant.event.current_record:
-                    record, created = Record.objects.get_or_create(
-                        event=event_participant.event,
-                        participant=event_participant.participant,
-                        record=event_participant.best_attempt,
-                        record_date=timezone.now()
-                    )
-                    record.save()
-                    event_participant.event.current_record_holder_name = f'{event_participant.participant.first_name} {event_participant.participant.last_name}'
-                    event_participant.event.current_record = event_participant.best_attempt
-                    event_participant.event.record_date = timezone.now()
-                    event_participant.event.save()
-                    event_participant.participant.individual_points += 10
-                    event_participant.participant.competitive_house.points += 10
-                else:
-                    event_participant.participant.individual_points += 9
-                    event_participant.participant.competitive_house.points += 9
-            elif event_participant.athlete_position == 2:
-                event_participant.participant.individual_points += 7
-                event_participant.participant.competitive_house.points += 7
-            elif event_participant.athlete_position == 3:
-                event_participant.participant.individual_points += 6
-                event_participant.participant.competitive_house.points += 6
-            elif event_participant.athlete_position == 4:
-                event_participant.participant.individual_points += 5
-                event_participant.participant.competitive_house.points += 5
-            elif event_participant.athlete_position == 5:
-                event_participant.participant.individual_points += 4
-                event_participant.participant.competitive_house.points += 4
-            elif event_participant.athlete_position == 6:
-                event_participant.participant.individual_points += 3
-                event_participant.participant.competitive_house.points += 3
-            elif event_participant.athlete_position == 7:
-                event_participant.participant.individual_points += 2
-                event_participant.participant.competitive_house.points += 2
-            elif event_participant.athlete_position == 8:
-                event_participant.participant.individual_points += 1
-                event_participant.participant.competitive_house.points += 1
+        # **Bulk update to save all rankings in a single query**
+        EventParticipation.objects.bulk_update(all_event_participants, ['athlete_position'])
+
+        # **Update points for top 8 positions**
+        points_map = {1: 9, 2: 7, 3: 6, 4: 5, 5: 4, 6: 3, 7: 2, 8: 1}
+        participants_to_update = []
+
+        for event_participant in all_event_participants:
+            position = event_participant.athlete_position
+
+            if position == 1 and event_participant.best_attempt > event_participant.event.current_record:
+                record, created = Record.objects.get_or_create(
+                    event=event_participant.event,
+                    participant=event_participant.participant,
+                    record=event_participant.best_attempt,
+                    record_date=timezone.now()
+                )
+                event_participant.event.current_record_holder_name = (
+                    f'{event_participant.participant.first_name} {event_participant.participant.last_name}'
+                )
+                event_participant.event.current_record = event_participant.best_attempt
+                event_participant.event.record_date = timezone.now()
+                event_participant.event.save()
+                points = 10  # Ten points for broken record
+
+            # Update individual and house points
+                event_participant.participant.individual_points += points
+                event_participant.participant.competitive_house.points += points
+            
             else:
-                event_participant.participant.individual_points += 0
-                event_participant.participant.competitive_house.points += 0
 
-            # Save the participant and house points without calling save on EventParticipation again
-            event_participant.participant.save()
-            event_participant.participant.competitive_house.save()
-    
-    # validate age group when model is created
+                points = points_map.get(position, 0)  # Default to 0 if out of top 8
 
+            # Update individual and house points
+                event_participant.participant.individual_points += points
+                event_participant.participant.competitive_house.points += points
+
+            participants_to_update.append(event_participant.participant)
+            participants_to_update.append(event_participant.participant.competitive_house)
+
+            # **Check for new event record**
+            
+
+        # **Bulk update participant and house points in a single query**
+        Participant.objects.bulk_update(
+            [p for p in participants_to_update if isinstance(p, Participant)], 
+            ['individual_points']
+        )
+        CompetitiveHouse.objects.bulk_update(
+            [p for p in participants_to_update if isinstance(p, CompetitiveHouse)], 
+            ['points']
+        )
 
     def __str__(self):
         return f'{self.participant} - {self.event}'
+
     
 # post_save function to rank participants in an event and assign points to house and individual
