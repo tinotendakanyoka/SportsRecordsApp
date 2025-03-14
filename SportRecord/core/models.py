1ebdd61194cc344426d4ac09dd65980dce32514c
Tinotenda Kanyoka <tkanyoka01@gmail.com>
2025-03-14 09:41:24 +0200

    Gitignore

--- SportRecord/core/models.py
+++ SportRecord/core/models.py
@@ -1,252 +1,144 @@
 from django.db import models
 from django.utils import timezone
-import datetime
-from datetime import datetime
-from django.db.models.signals import pre_save
-from django.forms import ChoiceField
-from django.db.models import Q
 
-AGE_GROUPS = (
-    ("U14", "U14"),
-    ("U15", "U15"),
-    ("U16", "U16"),
-    ("U17", "U17"),
-    ("U18", "U18"),
-    ("U20", "U20"),
-    ("OPEN", "OPEN"),
-
-)
+# Create your models here.
 
 class AgeGroup(models.Model):
-    name = models.CharField(max_length=255, choices=AGE_GROUPS)
+    title = models.CharField(max_length=255, null=True, blank=True, default='')
+    earliest_dob = models.DateField()
+    gender = models.CharField(max_length=1, choices=[
+        ('M', 'Male'),
+        ('F', 'Female')])
+
+    def __str__(self):
+        return f'{self.title} Boys' if self.gender == 'M' else f'{self.title} Girls'
+
+class CompetitiveHouse(models.Model):
+    name = models.CharField(max_length=255)
+    points = models.IntegerField(default=0)
+    color = models.CharField(max_length=255, default='')
 
     def __str__(self):
         return self.name
 
-# Create your models here.
-class Event(models.Model):
-    name = models.CharField(primary_key=True, max_length=255, blank=True)
-    event_num = models.IntegerField(blank=False)   #, unique=True
-    is_boys_event = models.BooleanField(default=False)  
-    event_year = models.IntegerField(default=timezone.now().year)
 
-class AgeGroupEvent(models.Model):
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+class AthleticEvent(models.Model):
+    title = models.CharField(max_length=255)
     age_group = models.ForeignKey(AgeGroup, on_delete=models.CASCADE)
+    current_record_holder_name = models.CharField(max_length=255)
+    current_record = models.FloatField()
+    record_date = models.DateField()
+    is_track_event = models.BooleanField(default=False)
 
-    
-    def __str__(self):
-        return f"{self.event.name} {self.age_group.name}"
-    
-
-HOUSE_CHOICES = (
-    ("MICHAEL", "MICHAEL"),
-    ("GABRIEL", "GABRIEL"),
-    ("RAPHAEL", "RAPHAEL"),
-)
-
-class House(models.Model):
-    name = models.CharField(max_length=255, choices=HOUSE_CHOICES)
-    points = models.FloatField(default=0, null=False)
-    house_color = models.CharField(max_length=25, null=False, blank=False, default='red')
-    position = models.IntegerField(default=4)
-
-
-    def determine_position(self):
-        ordered_houses_queryset = enumerate(self.objects.order_by("-points"), start=1)
-
-        for index, house in ordered_houses_queryset:
-            house.position == index
-            house.save()
+    class Meta:
+        unique_together = ['title', 'age_group']
 
     def __str__(self):
-        return f'{self.name}: {self.points} Points'
+        return f'{self.age_group} {self.title}'
 
-        
-GENDER_CHOICES = (
-    ("MALE", "MALE"),
-    ("FEMALE", "FEMALE"),
-)
 
-class Student(models.Model):
-    full_name = models.CharField(max_length=255)
+
+
+class Participant(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
     date_of_birth = models.DateField()
-    gender = models.CharField(choices = GENDER_CHOICES, max_length=10)
-    points = models.IntegerField(default=0)
-    house = models.ForeignKey(House, on_delete=models.CASCADE, related_name='students')
-    
-    
+
+    gender = models.CharField(max_length=1, choices=[
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ])
+    competitive_house = models.ForeignKey(CompetitiveHouse, on_delete=models.CASCADE)
+    individual_points = models.IntegerField(default=0)
+
     def __str__(self):
-        return f"{self.full_name}"
-    
+        return f'{self.first_name} {self.last_name}'
+
 
 class Record(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='records')
-    event = models.OneToOneField(AgeGroupEvent, on_delete=models.CASCADE, related_name='record')
-    time_or_distance = models.CharField(max_length=255)
-    event_year = models.IntegerField(default=timezone.now().year)
+    event = models.OneToOneField(AthleticEvent, on_delete=models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    record = models.FloatField()
+    record_date = models.DateField()
 
     def __str__(self):
-        return f'{self.student.full_name} - {self.event.event.name} : {self.time_or_distance}'
-    
+        return f'{self.participant} - {self.event} - {self.record}'
     
 
 class EventParticipation(models.Model):
-    id = models.AutoField(primary_key=True)
-    event = models.ForeignKey(AgeGroupEvent, on_delete=models.CASCADE, blank=True, related_name='events')
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, blank=True, related_name='event')
-    attempt1  = models.CharField(max_length=255, default='0m')
-    attempt2  = models.CharField(max_length=255, default='0m')
-    attempt3  = models.CharField(max_length=255, default='0m')
-    laptime_or_distance = models.CharField(max_length=255, default='0m')
-    position = models.IntegerField(default=0)
+    event = models.ForeignKey(AthleticEvent, on_delete=models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    attempt1 = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    attempt2 = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    attempt3 = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
 
-    
-    
+    best_attempt = models.FloatField()
+    athlete_position = models.IntegerField(default=6)
+
     class Meta:
-        unique_together = (('event', 'student'),)  
+        unique_together = ['event', 'participant']
+
+    def save(self, *args, **kwargs):
+        self.best_attempt = max(self.attempt1, self.attempt2, self.attempt3)
+        # update athlete position based on best attempt
+        all_event_participants = EventParticipation.objects.filter(event=self.event).order_by('-best_attempt')
+        for index, event_participant in enumerate(all_event_participants):
+            event_participant.athlete_position = index + 1
+            # update points for house and individual
+            match event_participant.athlete_position:
+                case 1:
+                    if event_participant.best_attempt > event_participant.event.current_record:
+                        record, created = Record.objects.get_or_create(
+                            event=event_participant.event,
+                            participant=event_participant.participant,
+                            record=event_participant.best_attempt,
+                            record_date=timezone.now()
+                        )
+                        record.save()
+                        event_participant.event.current_record_holder_name = event_participant.participant.first_name + ' ' + event_participant.participant.last_name
+                        event_participant.event.current_record = event_participant.best_attempt
+                        event_participant.event.record_date = timezone.now()
+                        event_participant.event.save()
+                        event_participant.participant.individual_points += 10
+                        event_participant.participant.competitive_house.points += 10
+                    else:
+                        event_participant.participant.individual_points += 9
+                        event_participant.participant.competitive_house.points += 9
+                case 2:
+                    event_participant.participant.individual_points += 7
+                    event_participant.participant.competitive_house.points += 7
+                case 3:
+                    event_participant.participant.individual_points += 6
+                    event_participant.participant.competitive_house.points += 6
+                case 4:
+                    event_participant.participant.individual_points += 5
+                    event_participant.participant.competitive_house.points += 5
+                case 5:
+                    event_participant.participant.individual_points += 4
+                    event_participant.participant.competitive_house.points += 4
+                case 6:
+                    event_participant.participant.individual_points += 3
+                    event_participant.participant.competitive_house.points += 3
+                case 7:
+                    event_participant.participant.individual_points += 2
+                    event_participant.participant.competitive_house.points += 2
+                case 8:
+                    event_participant.participant.individual_points += 1
+                    event_participant.participant.competitive_house.points += 1
+                case _:
+                    event_participant.participant.individual_points += 0
+                    event_participant.participant.competitive_house.points += 0
+            event_participant.save()
+
+        
+        super().save(*args, **kwargs)
+    
+    # validate age group when model is created
+
 
     def __str__(self):
-        return f"{self.student.full_name} participating in {self.event.name}"
-
-
-    def calculate_position(self):
-        """Calculates and updates the position based on attempts."""
-        attempts = [self.attempt1, self.attempt2, self.attempt3]
-        best_attempt = max(attempts)
-        print(best_attempt)
-        self.laptime_or_distance = str(best_attempt)  # Convert to string
-
-
-        rankings = EventParticipation.objects.filter(event=self.event).order_by('-laptime_or_distance')
-        for i, obj in enumerate(rankings, start=1):
-            if obj == self:
-                self.position = i
-                break
-
-
-
-        # # Rank positions based on best attempts (lower is better)
-        # rankings = EventParticipation.objects.filter(event=self.event).order_by('laptime_or_distance')
-        # self.position = rankings.index(self) + 1  # Add 1 for starting from 1
-
-        self.save()
-
-    # def __str__(self):
-    #     return f"{self.student} - {self.laptime_or_distance}"
-
-def validate_eventparticipation(sender,instance, **kwargs):    
-
-    studentperson = instance.student
-    eventqualified = instance.event
-    pos_attained = int(instance.position)
-    laptime_distance = instance.laptime_or_distance  
-   
-         
-    hsepts =0  
+        return f'{self.participant} - {self.event}'
     
-    if pos_attained == 1:
-        hsepts = 6
-    elif pos_attained == 2:
-        hsepts = 5
-    elif pos_attained == 3:
-        hsepts = 4
-    elif pos_attained == 4:
-        hsepts = 3
-    elif pos_attained == 5:
-        hsepts = 2
-    elif pos_attained == 6:
-        hsepts = 1
-        
-    current_student_pts =  int(studentperson.points)
-    points_update = current_student_pts + hsepts 
-    Student.objects.filter(full_name=studentperson.full_name).update(points=points_update)
-    print(studentperson.full_name, '  now has ',studentperson.points, ' points' )
-       
-      
-    current_house_pts =  studentperson.house.points
-    house_points_update = current_house_pts + hsepts 
-    House.objects.filter(name=studentperson.house.name).update(points=house_points_update)
-    print(studentperson.house.name, '  now has ',studentperson.house.points , ' points' )
-    # else:
-    #     current_gpts =  studentperson.hse.girl_pts
-    #     g_points_update = current_gpts + hsepts 
-    #     House.objects.filter(name=studentperson.hse.name).update(girl_pts=g_points_update)
-    #     print(studentperson.hse.name, '  now has ',studentperson.hse.girl_pts , ' points' )
-        
-
-    #Capturing Current Record
-    current_laptime_distance = eventqualified.record.time_or_distance
-    if "m" in current_laptime_distance:
-        record_distance = current_laptime_distance
-    else:
-        record_time = current_laptime_distance
-        print(record_time, ' is the record time')
-        
-    
-    
-    
-    
-    if "m" in laptime_distance:
-          # Distance input (in meters)
-          try:
-            laptime_distance.replace(",", ".")
-            user_distance = float(laptime_distance.split("m")[0])
-            record_distance = float(record_distance.split("m")[0])
-            
-            if user_distance > record_distance:
-              message = "Congratulations! new record"
-              Record.objects.filter(event=eventqualified.event.name).update(description=studentperson.full_name)
-              Record.objects.filter(event=eventqualified.event.name).update(time_or_distance=laptime_distance)
-              Record.objects.filter(event=eventqualified.event.name).update(event_year=timezone.now().year)
-              Record.objects.filter(event=eventqualified.event.name).update(student=studentperson)
-
-
-
-
-          except ValueError:
-            message = "Invalid input for distance. Please enter a number followed by 'm'."
-    
-    elif ":" in laptime_distance:
-      # Time input (in minutes:seconds)
-     
-            
-     # try:
-   
-    
-        user_minutesR, user_seconds_with_millisecondsR = record_time.split(":")
-        print(user_minutesR, user_seconds_with_millisecondsR, ' split time')
-        user_secondsR, user_millisecondsR = user_seconds_with_millisecondsR.split(".")
-        user_timeR = datetime.strptime(f"{user_minutesR}:{user_secondsR}.{user_millisecondsR}", "%M:%S.%f").time()
-        print(record_time, ' is the record time and splits  ', user_timeR)  
-        
-        
-        print(laptime_distance, '   is the laptop distance')
-    
-        user_minutes, user_seconds_with_milliseconds = laptime_distance.split(":")
-        user_seconds, user_milliseconds = user_seconds_with_milliseconds.split(".")
-        
-        user_time = datetime.strptime(f"{user_minutes}:{user_seconds}.{user_milliseconds}", "%M:%S.%f").time()
-        print(user_time, ' is the user time and splits  ', user_time)  
-      
-        if user_time < user_timeR:
-            # time_difference = record_time - user_time
-            # minutes, seconds = divmod(time_difference.seconds, 60)
-            # milliseconds = time_difference.microseconds // 1000   
-            Record.objects.filter(event=eventqualified.event.name).update(name=studentperson.full_name)
-            Record.objects.filter(event=eventqualified.event.name).update(time_or_distance=user_time)
-            Record.objects.filter(event=eventqualified.event.name).update(event_year=timezone.now().year)
-           # Record.objects.filter(event=eventqualified.record.event).update(event_year=timezone.now().year)
-    
-    
-            
-            message = "Congratulations"
-        else:
-            message = "The record time is {}. Try again!".format(record_time)
-          
-      # except ValueError:
-      #     message = "Invalid input for time. Please enter in minutes:seconds format (e.g., 00:10.00)."
-
-
-        
-pre_save.connect(validate_eventparticipation, sender=EventParticipation)
+# post_save function to rank participants in an event and assign points to house and individual
--- SportRecord/core/utils.py
+++ SportRecord/core/utils.py
@@ -1,70 +1,73 @@
-import csv
-from datetime import datetime
+import pandas as pd
+from .models import Participant, CompetitiveHouse, AgeGroup
 
-from .models import GENDER_CHOICES, Student, House
+def get_student_info_from_csv(file_path):
+    return pd.read_csv(file_path)
 
-def convert_date(date_str):
- """Converts a date string in DD/MM/YY format to YYYY-MM-DD format.
+def initialize_data():
 
- This function splits the string at '/', reverses the order of the elements,
- and joins them back with '-' to create a string in YYYY-MM-DD format.
+    age_groups = [
+        {
+            'title': 'Under 14',
+            'earliest_dob': '2012-01-01',
+        },
+        {
+            'title': 'Under 15',
+            'earliest_dob': '2011-01-01',
 
- Args:
-     date_str: The date string to be converted (e.g., '24/03/2008').
-
- Returns:
-     The converted date string in YYYY-MM-DD format (e.g., '2008-03-24'), or
-     None if the string is not in the expected format.
- """
-
- try:
-   # Split the string at '/'
-   date_parts = date_str.split("/")
-   if len(date_parts) != 3:
-     return None  # Not in expected format (DD/MM/YY)
-
-   # Reverse the order of elements
-   date_parts.reverse()
-
-   # Join the parts with '-'
-   return "-".join(date_parts)
- except ValueError:
-   # Handle potential errors (e.g., invalid characters)
-   print(f"Invalid date format: {date_str}")
-   return None
-
-
-def convert_gender(gender):
-  """Converts single letter gender (M/F) to model's choice value (MALE/FEMALE)."""
-  return GENDER_CHOICES[0][0] if gender.upper() == "M" else GENDER_CHOICES[1][0]
-
-
-def create_students_from_csv(csv_filename):
-  """Reads a CSV file and creates Student objects for each entry."""
-  with open(csv_filename, newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-      # Extract data from each row (assuming header matches your CSV)
-      surname = row['Surname']
-      first_name = row['First name']
-      birth_date = convert_date(row['Birth date'])
-      gender = convert_gender(row['Gender'])
-      house_name = row['Competitive house']  # Assuming house is identified by name
-
-      # Find the house object (replace with your logic to find the house)
-      house = House.objects.get(name=house_name)
-
-      # Create and save the Student object
-      student = Student(
-          full_name=f"{first_name} {surname}",
-          date_of_birth=birth_date,
-          gender=gender,
-          points=0,  # Assuming default points is 0
-          house=house,
-      )
-      student.save()
-
-  print(f"Successfully created student objects from {csv_filename}.")
+        },
+        {
+            'title': 'Under 16',
+            'earliest_dob': '2010-01-01',
+        },
+        {
+            'title': 'Under 17',
+            'earliest_dob': '2009-01-01',
+        },
+        {
+            'title': 'Under 18',
+            'earliest_dob': '2008-01-01',
+        },
+        {
+            'title': 'OPEN',
+            'earliest_dob': '2007-01-01',
+        },
+    ]
 
 
 
+
+    df = pd.DataFrame(get_student_info_from_csv('data/students.csv'))
+
+    for index, row in df.iterrows():
+        # determine empty 
+        first_name = row['First name']
+        last_name = row['Surname']
+        date_of_birth = str(row['Birth date'])
+        gender = row['Gender']
+
+        house = row['Competitive hosue']
+
+        house, created = CompetitiveHouse.objects.get_or_create(name=house)
+
+        participant, created = Participant.objects.get_or_create(
+            first_name=first_name,
+            last_name=last_name,
+            date_of_birth=date_of_birth,
+            gender=gender,
+            competitive_house=house
+        )
+
+    genders = ['M', 'F']
+
+    for gender in genders:
+        for age_group in age_groups:
+            title = age_group['title']
+            earliest_dob = age_group['earliest_dob']
+
+            AgeGroup.objects.get_or_create(
+                title=title,
+                earliest_dob=earliest_dob,
+                gender=gender)
+
+    print('Data has been successfully imported')
