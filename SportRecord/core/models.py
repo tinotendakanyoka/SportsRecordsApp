3c51354c58179e1fa6c073df27ec76bb8ac25ccf
Tinotenda Kanyoka <tkanyoka01@gmail.com>
2025-03-14 23:10:07 +0200

    No Bonus for record, yet

--- SportRecord/core/models.py
+++ SportRecord/core/models.py
@@ -83,6 +83,22 @@ class EventParticipation(models.Model):
     class Meta:
         unique_together = ['event', 'participant']
 
+    from django.db import models
+from django.utils import timezone
+
+class EventParticipation(models.Model):
+    event = models.ForeignKey(AthleticEvent, on_delete=models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    attempt1 = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    attempt2 = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    attempt3 = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+
+    best_attempt = models.FloatField(default=0.00)
+    athlete_position = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ['event', 'participant']
+
     def save(self, *args, **kwargs):
         """ Update best attempt before saving """
         self.best_attempt = max(self.attempt1, self.attempt2, self.attempt3)
@@ -99,12 +115,21 @@ class EventParticipation(models.Model):
         EventParticipation.objects.bulk_update(all_event_participants, ['athlete_position'])
 
         # **Update points for top 8 positions**
-        points_map = {1: 9, 2: 7, 3: 6, 4: 5, 5: 4, 6: 3, 7: 2, 8: 1}
+        points_map = {1: 10, 2: 7, 3: 6, 4: 5, 5: 4, 6: 3, 7: 2, 8: 1}
         participants_to_update = []
 
         for event_participant in all_event_participants:
             position = event_participant.athlete_position
+            points = points_map.get(position, 0)  # Default to 0 if out of top 8
 
+            # Update individual and house points
+            event_participant.participant.individual_points += points
+            event_participant.participant.competitive_house.points += points
+
+            participants_to_update.append(event_participant.participant)
+            participants_to_update.append(event_participant.participant.competitive_house)
+
+            # **Check for new event record**
             if position == 1 and event_participant.best_attempt > event_participant.event.current_record:
                 record, created = Record.objects.get_or_create(
                     event=event_participant.event,
@@ -118,25 +143,6 @@ class EventParticipation(models.Model):
                 event_participant.event.current_record = event_participant.best_attempt
                 event_participant.event.record_date = timezone.now()
                 event_participant.event.save()
-                points = 10  # Ten points for broken record
-
-            # Update individual and house points
-                event_participant.participant.individual_points += points
-                event_participant.participant.competitive_house.points += points
-            
-            else:
-
-                points = points_map.get(position, 0)  # Default to 0 if out of top 8
-
-            # Update individual and house points
-                event_participant.participant.individual_points += points
-                event_participant.participant.competitive_house.points += points
-
-            participants_to_update.append(event_participant.participant)
-            participants_to_update.append(event_participant.participant.competitive_house)
-
-            # **Check for new event record**
-            
 
         # **Bulk update participant and house points in a single query**
         Participant.objects.bulk_update(
@@ -147,6 +153,10 @@ class EventParticipation(models.Model):
             [p for p in participants_to_update if isinstance(p, CompetitiveHouse)], 
             ['points']
         )
+
+    def __str__(self):
+        return f'{self.participant} - {self.event}'
+
 
     def __str__(self):
         return f'{self.participant} - {self.event}'
