8235dbc02109f08123e2d49f77cecf83e10763a1
Tinotenda Kanyoka <tkanyoka01@gmail.com>
2025-03-14 23:23:01 +0200

    utils

--- SportRecord/core/utils.py
+++ SportRecord/core/utils.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from .models import Participant, CompetitiveHouse, AgeGroup
-from reportlab.lib.pagesizes import A4, portrait
+from reportlab.lib.pagesizes import A5, landscape
 from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import mm, inch
@@ -8,6 +8,57 @@ from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, 
 from reportlab.lib.enums import TA_CENTER, TA_RIGHT
 from django.http import HttpResponse
 from django.utils import timezone
+from .models import EventParticipation, AthleticEvent, Record, Participant, CompetitiveHouse
+
+def bulk_upd(event):
+    all_event_participants = list(EventParticipation.objects.filter(event=event).order_by('-best_attempt'))
+        
+        # Assign rankings
+    for index, event_participant in enumerate(all_event_participants):
+        event_participant.athlete_position = index + 1
+
+        # **Bulk update to save all rankings in a single query**
+    EventParticipation.objects.bulk_update(all_event_participants, ['athlete_position'])
+
+        # **Update points for top 8 positions**
+    points_map = {1: 9, 2: 7, 3: 6, 4: 5, 5: 4, 6: 3, 7: 2, 8: 1}
+    participants_to_update = []
+
+    for event_participant in all_event_participants:
+        position = event_participant.athlete_position
+        points = points_map.get(position, 0)  # Default to 0 if out of top 8
+
+        # Update individual and house points
+        event_participant.participant.individual_points += points
+        event_participant.participant.competitive_house.points += points
+
+        participants_to_update.append(event_participant.participant)
+        participants_to_update.append(event_participant.participant.competitive_house)
+
+        # **Check for new event record**
+        if position == 1 and event_participant.best_attempt > event_participant.event.current_record:
+            record, created = Record.objects.get_or_create(
+                event=event_participant.event,
+                participant=event_participant.participant,
+                record=event_participant.best_attempt,
+                record_date=timezone.now()
+            )
+            event_participant.event.current_record_holder_name = (
+                f'{event_participant.participant.first_name} {event_participant.participant.last_name}'
+            )
+            event_participant.event.current_record = event_participant.best_attempt
+            event_participant.event.record_date = timezone.now()
+            event_participant.event.save()
+
+        # **Bulk update participant and house points in a single query**
+    Participant.objects.bulk_update(
+        [p for p in participants_to_update if isinstance(p, Participant)], 
+        ['individual_points']
+    )
+    CompetitiveHouse.objects.bulk_update(
+        [p for p in participants_to_update if isinstance(p, CompetitiveHouse)], 
+        ['points']
+    )
 
 def get_student_info_from_csv(file_path):
     return pd.read_csv(file_path)
@@ -84,14 +135,14 @@ def generate_report(event):
     response = HttpResponse(content_type='applicaiton/pdf')
     response['Content-Disposition'] = f'attachment; filename="{event}.pdf"'
 
-    doc = SimpleDocTemplate(response, pagesize=portrait(A4))
+    doc = SimpleDocTemplate(response, pagesize=landscape(A5))
 
     elements = []
 
     styles = getSampleStyleSheet()
 
     doc.leftMargin = 50
-    doc.topMargin = 15
+    doc.topMargin = 3
 
     logo = 'data/logo.png'
     img = Image(logo, width=30*mm, height=30*mm)
@@ -100,27 +151,29 @@ def generate_report(event):
     info_row = Paragraph("Hellenic Academy Official Results Sheet", title_style)
             
     elements.append(img)
-    elements.append(Spacer(1, 10))
+    elements.append(Spacer(1, 2))
     elements.append(info_row)
-    elements.append(Spacer(1, 30))
+    elements.append(Spacer(1, 2))
 
     event_info = Paragraph(f'Event: {event}', styles['Heading3'])
     elements.append(event_info)
-    elements.append(Spacer(1, 15))
+    elements.append(Spacer(1, 2))
     results_table_skel = [
         ['Position', 'Participant Name', 'House', 'Distance/Laptime'],
 
     ]
+    
+    participations = EventParticipation.objects.filter(event=event).order_by('athlete_position')
 
-    for participation in event.participations.all():
-        results_table_skel.append([f'{participation.athlete_position}', f'{participation.participant.first_name} {participation.participant.last_name}', f'{participation.participant.competitive_house.name}', f'{participation.best_attempt}'])
+    for participation in participations:
+        results_table_skel.append([f'{participation.athlete_position}', f'{participation.participant.first_name} {participation.participant.last_name}', f'{participation.participant.competitive_house.name}', f'{participation.best_attempt} {"s" if event.is_track_event else "m"}'])
 
     table_styling = TableStyle([
         ('BACKGROUND', (0,0), (-1, 0), colors.white),
         ('TEXTCOLOR', (0,0), (-1,0), colors.black),
         ('ALIGN', (0,0), (-1, -1), 'CENTER'),
         ('FONTNAME', (0,0), (-1, 0), 'Helvetica-Bold'),
-        ('FONTSIZE', (0,0), (-1,-1), 14.5),
+        ('FONTSIZE', (0,0), (-1,-1), 11.5),
         ('BOTTOMPADDING', (0,0), (-1,0), 12),
         ('GRID', (0,0), (-1,-1), 1, colors.black),
 
